5b0a398149070f10d2cb1cb3fbef2b1e6c9ebc29
Brad Johnson <bradsk88@gmail.com>
2019-03-22 18:39:41 -0600

    Created a tree structure from a list of package JSON

--- sort.js
+++ sort.js
@@ -1,26 +1,55 @@
-function createGraphNode(name, edges) {
-
+function partition(array, isValid) {
+    return array.reduce(([pass, fail], elem) => {
+        return isValid(elem) ? [[...pass, elem], fail] : [pass, [...fail, elem]];
+    }, [[], []]);
 }
 
 function sort(packages) {
-    const out = {};
+
+    return flattenTree([], tree);
+}
+
+function buildTree(packages) {
+    const tree = {};
     const pkgNames = packages.map(p => p['name']);
     packages.forEach(pkg => {
         const peerDeps = pkg['peerDependencies'] || [];
+        const pName = pkg['name'];
+        if (!tree.hasOwnProperty(pName)) {
+            tree[pName] = {
+                users: [],
+                isLeaf: true,
+            }
+        }
         peerDeps.forEach(p => {
             if (!pkgNames.find(n => n === p)) {
-                console.log(`${p} is not in the set of packages being considered.  Skipping`)
+                console.log(`${p} is not in the set of packages being considered.  Skipping`);
                 return;
             }
-            if (out.hasOwnProperty(p)) {
-                out[p]['edges'].push(pkg['name']);
+            if (tree.hasOwnProperty(p)) {
+                tree[p].users.push(pName);
+                tree[p].isLeaf = false;
+            } else {
+                tree[p] = {
+                    users: [pName],
+                    isLeaf: false,
+                }
             }
         });
-        out[pkg['name']] = {
-            edges: [],
-        }
-    });
 
+    });
+    return tree;
 }
 
-module.exports = sort;
+function flattenTree(leaves, tree) {
+    if (Object.keys(tree).length === 0) {
+        return leaves;
+    }
+    console.log("Before partition: ", tree);
+    const [l, t] = partition(tree, o => o.isLeaf);
+    console.log("Leaves: ", l);
+    console.log("Tree: ", t);
+    return flattenTree(leaves + l, t);
+}
+
+export { sort, buildTree };
--- sort.spec.js
+++ sort.spec.js
@@ -1,4 +1,4 @@
-const sort = require('./sort');
+import {sort, buildTree} from './sort';
 
 test('should return an empty list unchanged', () => {
     const actual = sort([]);
@@ -44,5 +44,125 @@ test('should put dependency first (2 packages already in wrong order)', () => {
     };
     const actual = sort([pkgWithDeps, depPkg]);
     const expected = [depPkg, pkgWithDeps];
+    expect(actual).toEqual(expected);
+});
+
+test('buildTree should build correct tree for 2 packages with 1 dependency relationship', () => {
+    const pkgWithDeps = {
+        name: "HASDEPS",
+        peerDependencies: [
+            "DEP",
+        ],
+    };
+    const depPkg = {
+        name: "DEP",
+        peerDependencies: [],
+    };
+    const actual = buildTree([depPkg, pkgWithDeps]);
+    const expected = {
+        'DEP': {
+            users: ['HASDEPS'],
+            isLeaf: false,
+        },
+        'HASDEPS': {
+            users: [],
+            isLeaf: true,
+        },
+    };
+    expect(actual).toEqual(expected);
+});
+
+test('buildTree should build correct tree for 2 packages with 1 dependency relationship', () => {
+    const pkgWithDeps = {
+        name: "HASDEPS",
+        peerDependencies: [
+            "DEP",
+        ],
+    };
+    const depPkg = {
+        name: "DEP",
+        peerDependencies: [],
+    };
+    const actual = buildTree([depPkg, pkgWithDeps]);
+    const expected = {
+        'DEP': {
+            users: ['HASDEPS'],
+            isLeaf: false,
+        },
+        'HASDEPS': {
+            users: [],
+            isLeaf: true,
+        },
+    };
+    expect(actual).toEqual(expected);
+});
+
+test('buildTree should build correct tree for 3 packages with 2 linear relationships', () => {
+    const pkgWithDeps = {
+        name: "HASDEPS",
+        peerDependencies: [
+            "DEP",
+        ],
+    };
+    const pkg2WithDeps = {
+        name: "DEP",
+        peerDependencies: [
+            "DEEPDEP",
+        ],
+    };
+    const depPkg = {
+        name: "DEEPDEP",
+        peerDependencies: [],
+    };
+    const actual = buildTree([depPkg, pkgWithDeps, pkg2WithDeps]);
+    const expected = {
+        'DEEPDEP': {
+            users: ['DEP'],
+            isLeaf: false,
+        },
+        'DEP': {
+            users: ['HASDEPS'],
+            isLeaf: false,
+        },
+        'HASDEPS': {
+            users: [],
+            isLeaf: true,
+        },
+    };
+    expect(actual).toEqual(expected);
+});
+
+test( 'buildTree should build correct tree for 3 packages with in binary relationship', () => {
+    const pkgWithDeps = {
+        name: "HASDEPS",
+        peerDependencies: [
+            "DEP",
+        ],
+    };
+    const pkg2WithDeps = {
+        name: "HASDEPS2",
+        peerDependencies: [
+            "DEP",
+        ],
+    };
+    const depPkg = {
+        name: "DEP",
+        peerDependencies: [],
+    };
+    const actual = buildTree([depPkg, pkgWithDeps, pkg2WithDeps]);
+    const expected = {
+        'HASDEPS2': {
+            users: [],
+            isLeaf: true,
+        },
+        'DEP': {
+            users: ['HASDEPS', 'HASDEPS2'],
+            isLeaf: false,
+        },
+        'HASDEPS': {
+            users: [],
+            isLeaf: true,
+        },
+    };
     expect(actual).toEqual(expected);
 });
